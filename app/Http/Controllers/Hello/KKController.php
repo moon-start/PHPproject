db7450207f43f262a0ce4943a4cc8bb000d6ef92
moon-start <login0516mp4@gmail.com>
2020-01-16 21:03:06 +0800

    All File

---
+++ app/Http/Controllers/Hello/KKController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Hello;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KKController extends Controller
+{
+    //
+
+    public function index(){
+        return 'Hello目錄 ,KK Controller';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 
 ## view(導向)
+## 專案\resources\views\welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,9 +27,16 @@ Route::get('/', function () {
 ### URL目錄 - 一個name變數
 ### 預設值 moon
 Route::get('user/{name?}', function ($name = 'moon') {
-    return 'hello'.$name;
+    return 'hello , '.$name;
 });
 
 
-
+## QQ123目錄
+## 使用..index()
 Route::get('QQ123', 'QQController@index');
+
+
+## Controllers目錄底下
+## 新增一個 Hello目錄+ php
+## 專案> php artisan make:controller Hello/KKController
+Route::get('hello','Hello\KKController@index');
