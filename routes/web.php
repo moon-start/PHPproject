f0e0faf25d647d7cf6b2b89267ff023885910782
moon-start <login0516mp4@gmail.com>
2020-02-19 16:37:39 +0800

    All File

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::resource('customer', 'CustomerController');
+Route::get('customer','CustomerController@index');
+Route::get('new','CustomerController@new');
+Route::post('store','CustomerController@store');
+Route::get('edit','CustomerController@edit');
+Route::post('update','CustomerController@update');
+Route::get('delete','CustomerController@delete');
