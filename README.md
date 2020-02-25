eed5d15e6a81af2e5d9ee13a95478a9a1de5ce15
moon-start <login0516mp4@gmail.com>
2020-02-25 17:34:44 +0800

    EE

---
+++ README.md
@@ -0,0 +1,3 @@
+### Laravel 練習
+#### 綜合練習(一) [內容](exercise1/README.md)
+#### 綜合練習(二) [內容](exercise2/README.md)
---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Route;
+use View;
+use App\Customer;
+
+class CustomerController extends Controller
+{
+    //客戶列表
+    public function index() {
+        $customers = Customer::all();
+        return View::make('board',['customers' => $customers]);
+    }
+
+    //新增客戶資料
+    public function new() {
+        //$customers = Customer::all();
+        //return View::make('new',['customers' => $customers]);
+        return View::make('new');
+    }
+
+    //將新客戶資料寫入資料庫
+    public function store(Request $request){
+        if ($request->cancel){
+            $customers = Customer::all();
+            return View::make('board',['customers' => $customers]);
+        }
+        $customers = new Customer;
+        $customers->Cusid=$request->input('Cusid');
+        $customers->Name=$request->input('Name');
+        $customers->Address=$request->input('Address');
+        $customers->Phone=$request->input('Phone');
+        $customers->save();
+        return redirect('customer');
+    }
+
+    //修改客戶資料表格
+    public function edit(Request $request){
+        //$customers=$request->Cusid;
+        //dd($customers);
+        return View::make('edit',['Cusid'=>$request->Cusid,'Name'=>$request->Name,'Address'=>$request->Address,'Phone'=>$request->Phone]);
+    }
+
+    //更新客戶資料
+    public function update(Request $request){
+        if ($request->cancel){
+            $customers = Customer::all();
+            return View::make('board',['customers' => $customers]);
+        } 
+        $customers = Customer::where('Cusid',$request->input('oldId'))
+                                    ->update(['Cusid'=> $request->input('Cusid'),
+                                    'Name'=> $request->input('Name'),
+                                    'Address'=> $request->input('Address'),
+                                    'Phone'=> $request->input('Phone')
+        ]);
+        $customers = Customer::all();
+        return View::make('board',['customers' => $customers]); 
+    }
+
+    //刪除客戶資料
+    public function delete(Request $request){
+        Customer::where('Cusid',$request->input('Cusid'))->delete();
+        $customers = Customer::all();
+        return View::make('board',['customers' => $customers]); 
+    }
+}
